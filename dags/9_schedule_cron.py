bf5b98e3e5093e33aa8deed7f25af4c20a34257b
buivanthai <buithaild230899@gmail.com>
2026-03-07 15:40:43 +0700

    schedule-cron

--- dags/9_schedule_cron.py
+++ dags/9_schedule_cron.py
@@ -1,14 +1,17 @@
 from airflow.sdk import dag, task
 from pendulum import datetime
+from airflow.timetables.trigger import CronTriggerTimetable
 
 @dag(
-        dag_id = "first_schedule_dag",
-        start_date=datetime(year=2026, month=1, day=1, tz='Asia/Bangkok'),
-        schedule="@daily",
+        dag_id = "cron_schedule_dag",
+        start_date=datetime(year=2026, month=3, day=5, tz='Asia/Bangkok'),
+        # schedule=CronTriggerTimetable("0 15 * * *", timezone='Asia/Bangkok'), ## hang ngay vao luc 15h
+        schedule=CronTriggerTimetable("0 15 * * MON-FRI", timezone='Asia/Bangkok'), ## hang ngay vao luc 15h tu thu 2 den thu 6
+        end_date=datetime(year=2026, month=3, day=10, tz='Asia/Bangkok'),
         is_paused_upon_creation=False,
         catchup=True
 )
-def first_schedule_dag():
+def cron_schedule_dag():
     
     @task.python
     def first_task():
@@ -30,4 +33,4 @@ def first_schedule_dag():
     first >> second >> third
 
 # Instantiating the DAG
-first_schedule_dag()
+cron_schedule_dag()
